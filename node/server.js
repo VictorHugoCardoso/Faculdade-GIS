391431831c50c5ed28f201628fe8643ed53b56d3
victor <victor_mendes00@hotmail.com>
2019-05-28 14:33:28 -0300

    first

---
+++ node/server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+
+var express = require('express');
+var handlebars  = require('express-handlebars');
+var PORT = process.env.PORT || 3000;
+
+var app = express()
+	.engine('.hbs', handlebars({extname: '.hbs', cache: false}))
+	.set('view engine', '.hbs')
+	.use('/static', express.static('static'));
+
+
+app.listen(PORT);
+console.log('*------------------------------*');  
+console.log('Listening on port:' + PORT); 
+
+app.all('/', function (req, res) {
+	res.render('index.hbs');
+})
+
+app.get('/addLayer', function (req, res) {
+	res.send({mapid, token, computedIds});
+})
